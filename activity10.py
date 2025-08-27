2027b12f40c3927f1f94a9c7ebfe06af945cf70f
kasa137 <agrabiosokristin@gmail.com>
2025-08-27 15:34:55 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,13 @@
+print("Kristin likes sweets, her favorite drink is coke and donut for dessert\n")
+
+
+
+favoritedrink = "coke"
+favoritedessert = "donut"
+
+favdrink = input("What is Kristin's favorite drink? ")
+favdessert = input("what is Kristin's favorite dessert? ")
+if (favdrink == favoritedrink) and (favdessert == favoritedessert):
+	 print("CORRECT")
+else:
+	print("incorrect")
