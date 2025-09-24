8e85f37599d726f612bd7d305d80723d3c340e29
kasa137 <agrabiosokristin@gmail.com>
2025-09-24 12:10:32 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,10 @@
+print("STRING FORMATING")
+print("string concatenation")
+name = "Kristin Angela "
+mname = "Salazada"
+sname = "Agrabioso"
+print("USING f{} method")
+
+print(f"My full name is {name} {mname} {sname}\n")
+print("USING f{" ".upper} method")
+print(f"My full name is {name.upper} {mname.upper} {sname.upper}")
