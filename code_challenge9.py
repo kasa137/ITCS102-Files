a0ba6d37eef99f6156e179741fdc18c5e741d37c
kasa137 <agrabiosokristin@gmail.com>
2025-09-25 15:42:13 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,6 @@
+print("WELLCOME TO OLAF SIMULATOR - THE ECHO CHAMBER!!")
+say = input("What do you want Olaf to say?  ")
+x = eval(input("How many times should Olaf say it?  "))
+print("Listen to Olaf ganna say:")
+for yah in range(x,0,-1):
+	print(say)
