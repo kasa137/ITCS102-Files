77c03c58067110532912498c5f8aa21edabb8583
kasa137 <agrabiosokristin@gmail.com>
2025-10-25 16:20:15 +0800

    Add files via upload

---
+++ code_challenge12.py
@@ -0,0 +1,8 @@
+for unak in range(1,7,1):
+	for woo in range(7,unak,-1):
+		print(" ",end=" ")
+	for nak in range(unak,0,-1):
+		print(nak,end=" ")
+	for woonhak in range(2,unak+1,1):
+		print(woonhak,end=" ")
+	print()
