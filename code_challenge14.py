8a054286804bdde594d3d80711ceab0af868b7be
kasa137 <agrabiosokristin@gmail.com>
2025-10-25 13:50:16 +0800

    Add files via upload

---
+++ code_challenge14.py
@@ -0,0 +1,49 @@
+print("ENTER AN ANIME FOR AN ANIME LIST, ENTER 'exit' TO EXIT \n")
+unak = True
+ihan = 0
+ani = ['one piece','naruto','bleach','overlord','ghost fighter','flame of recca','zenki','cowboy bebop']
+while unak == True:
+	anime = input("Enter anime name (or type 'exit' to finish ) ----> ").lower()
+	if anime == ani[0]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[0]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[1]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[2]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[3]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[4]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[5]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[6]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == ani[7]:
+		ihan += 1
+		print(f"{anime}, has been added to your anime list")
+		continue
+	elif anime == 'exit':
+		print("here's your anime list ", ihan)
+		break
+		
+		
+	
+	
